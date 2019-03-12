5d64764a5943a787115f3e0f87ffb209dcaecb54
Hansin Susatya <hansinsusatya@gmail.com>
2019-03-12 13:47:48 +0700

    Setup user permission

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,5 @@ class Listing < ApplicationRecord
 
   validates :name, :description, :price, :image, presence: true
   validates :price, numericality: { greater_than: 0}
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_053348) do
+ActiveRecord::Schema.define(version: 2019_03_12_061147) do
 
   create_table "listings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_053348) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
